2017468fa0df94ae66284884220f11edc6505b89
Atul <atulsingh1396@gmail.com>
2020-04-13 13:21:01 +0530

    added delete functionality

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,34 +1,70 @@
 import React, { Fragment, useEffect, useState } from "react";
 
 const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  // fetch todos
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  // delete Todo
+  const deleteTodo = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "delete",
+      });
+      setTodos(todos.filter(todo => todo.todo_id !== id));
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
   return (
     <Fragment>
-      {/* <table class="table mt-5 text-center">
+      <table className="table mt-5 text-center">
         <thead>
           <tr>
             <th>Description</th>
             <th>Edit</th>
-            <th>Todo</th>
+            <th>Delete</th>
           </tr>
         </thead>
+
         <tbody>
-          <tr>
+          {todos.map((todo) => (
+            <tr key={todo.todo_id}>
+              <td>{todo.description}</td>
+              <td>
+                <button className="btn btn-dark">Edit</button>
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+          {/* <tr>
             <td>John</td>
             <td>Doe</td>
             <td>john@example.com</td>
-          </tr>
-          <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
-          </tr>
-          <tr>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
-          </tr>
+          </tr> */}
         </tbody>
-      </table> */}
+      </table>
     </Fragment>
   );
 };
